191791b488f52c488a42b84f9aca535ea2ba2b14
Pumble <juanlgu27@gmail.com>
2020-02-01 04:37:04 -0600

    union de los proyectos
    fog, audio y movimiento

---
+++ Assets/_scripts/treeMusic.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treeMusic : MonoBehaviour
+{
+	[SerializeField]
+	private AudioSource treeAudio;
+	[SerializeField]
+	private AudioSource ambientAudio;
+	[SerializeField]
+	private GameObject player;
+
+	private float distanceToPlayerInitial;
+	private float distanceToPlayer;
+	private bool inRadius = false;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag(player.tag) && !inRadius)
+		{
+			distanceToPlayerInitial = Vector3.Distance(player.transform.position, transform.position);
+			inRadius = true;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag(player.tag))
+		{
+			inRadius = false;
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.CompareTag(player.tag))
+		{
+			distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+
+			float percentageAmbient = treeMusic.percentageVolAmbient(distanceToPlayerInitial, distanceToPlayer);
+			ambientAudio.volume = percentageAmbient/100;
+			//Debug.Log(string.Format("Ambient {0}%", percentageAmbient));
+
+			float percentageTree = treeMusic.percentageVolTree(distanceToPlayerInitial, distanceToPlayer);
+			treeAudio.volume = percentageTree / 100;
+			Debug.Log(string.Format("Tree {0}%", percentageTree));
+
+		}
+	}
+
+	static float percentageVolAmbient(float initialDistance, float currentDistance)
+	{
+		float result = (currentDistance / initialDistance) * 100;
+		return result;
+	}
+
+	static float percentageVolTree(float initialDistance, float currentDistance)
+	{
+		float result = 100 - ((currentDistance / initialDistance) * 100);
+		return result;
+	}
+}
