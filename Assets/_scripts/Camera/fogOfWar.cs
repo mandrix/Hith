2818ff8c2b5096128591ab0f1a4eb98c96557460
JZ1999 <joseph.zamora64@yahoo.com>
2020-03-07 23:43:11 -0600

    Added a minimap with a fog of war feature

---
+++ Assets/_scripts/Camera/fogOfWar.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+// Creado por JZ1999
+
+public class fogOfWar : MonoBehaviour
+{
+	#region Variables
+	public GameObject m_fogOfWarPlane;
+	public Transform m_player;
+	public LayerMask m_fogLayer;
+	public float m_radius = 5f;
+	private float m_radiusSqr { get { return m_radius * m_radius; } }
+
+	private Mesh m_mesh;
+	private Vector3[] m_vertices;
+	private Color[] m_colors;
+	#endregion
+
+	#region Unity Methods
+	void Start()
+    {
+		Initialize();
+
+	}
+
+	void Update()
+    {
+		Ray r = new Ray(transform.position, m_player.position - transform.position);
+		RaycastHit hit;
+		if (Physics.Raycast(r, out hit, 1000, m_fogLayer, QueryTriggerInteraction.Collide))
+		{
+			for (int i = 0; i < m_vertices.Length; i++) {
+				Vector3 v = m_fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
+				float dist = Vector3.SqrMagnitude(v - hit.point);
+				if (dist < m_radiusSqr) {
+					float alpha = Mathf.Min(m_colors[i].a, dist / m_radiusSqr);
+					m_colors[i].a = alpha;
+				}
+			}
+			UpdateColor();
+		}
+	}
+	#endregion
+
+	#region Custom methods
+
+	void Initialize()
+	{
+		m_mesh = m_fogOfWarPlane.GetComponent < MeshFilter > ().mesh;
+		m_vertices = m_mesh.vertices;
+		m_colors = new Color[m_vertices.Length];
+		for (int i = 0; i < m_colors.Length; i++) {
+			m_colors[i] = Color.black;
+		}
+		UpdateColor();
+	}
+
+	void UpdateColor()
+	{
+		m_mesh.colors = m_colors;
+	}
+	#endregion
+}
---
+++ Assets/_scripts/Camera/followObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+// Creado por JZ1999
+
+public class followObject : MonoBehaviour
+{
+	#region Variables
+	[SerializeField]
+	[Tooltip("The transform to follow")]
+	private Transform objTransform;
+    #endregion
+
+    #region Unity Methods
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+		transform.position = new Vector3(objTransform.position.x, transform.position.y, objTransform.position.z);
+    }
+    #endregion
+
+    #region Custom methods
+
+    #endregion
+}
