ed327f8936ee112c416db3ae88c1dba1b227b5eb
mandrix <ale.aazv@icloud.com>
2020-03-18 02:51:58 -0600

    tutorial preview

---
+++ Assets/_scripts/Interactables/DestroyObjectTutorial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectTutorial : MonoBehaviour
+{
+    public GameObject window1;
+    public GameObject window2;
+    public GameObject window3;
+    public GameObject itemSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
