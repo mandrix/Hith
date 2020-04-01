73dcd478680dd472408df71fefc65822415eb27d
mandrix <ale.aazv@icloud.com>
2020-04-01 04:18:33 -0600

    new assets

---
+++ Assets/_scripts/Manager/MovementShip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocity;
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newTransform = new Vector3 (0,0,velocity);
+        transform.position += newTransform;
+        
+    }
+}
