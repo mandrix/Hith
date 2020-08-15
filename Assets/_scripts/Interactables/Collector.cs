36996c3ad729da5a80dc5e9ce024b639926d6c80
mandrix <ale.aazv@icloud.com>
2020-08-15 02:28:39 -0600

    collector script

--- Assets/_scripts/Interactables/Collector.cs
+++ Assets/_scripts/Interactables/Collector.cs
@@ -12,29 +12,24 @@ public class Collector : MonoBehaviour
     [SerializeField]
     private GameObject Collider;
 
-    private List<GameObject> colliders = new List<GameObject>(); 
+    private List<GameObject> colliders = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
         int index = 0;
         int maxLength = items.Length;
-        while (index != maxLength) {
-            GameObject item = items[index++]; 
+        while (index != maxLength)
+        {
+            GameObject item = items[index++];
             LocateColliderObject(item);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    } 
 
     private void LocateColliderObject(GameObject item) {
         Vector3 position = item.transform.position;
         GameObject newCollider = Instantiate(Collider, position, new Quaternion(), transform);
         newCollider.GetComponent<SpawnItem>().SetParent(item);
-        colliders.Add(newCollider);//error
+        colliders.Add(newCollider);
     }
 
 }
